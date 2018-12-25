37569a4cdbca0a25905cd9eb240d94f2fbe79f9c
Stephan Griesel <sgriesel@gmail.com>
2018-12-25 08:33:23 +0100

    start js

---
+++ script.js
@@ -0,0 +1,48 @@
+class Book {
+    constructor(title,author,isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+
+class UI {
+    static displayBooks(){
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '32311234'
+            },
+            {
+                title: 'Book Two',
+                author: 'Sarah Doe',
+                isbn: '23123134'
+            },
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks)

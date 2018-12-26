81043f71156fe3b3b442aa372ce47450f1cbc52c
Stephan Griesel <sgriesel@gmail.com>
2018-12-26 08:38:48 +0100

    add alert & timeout

--- script.js
+++ script.js
@@ -1,84 +1,96 @@
 class Book {
-    constructor(title,author,isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 }
 
-
 class UI {
-    static displayBooks(){
-        const StoredBooks = [
-            {
-                title: 'Book One',
-                author: 'John Doe',
-                isbn: '32311234'
-            },
-            {
-                title: 'Book Two',
-                author: 'Sarah Doe',
-                isbn: '23123134'
-            }
-        ];
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "32311234"
+      },
+      {
+        title: "Book Two",
+        author: "Sarah Doe",
+        isbn: "23123134"
+      }
+    ];
 
-        const books = StoredBooks;
+    const books = StoredBooks;
 
-        books.forEach((book) => UI.addBookToList(book));
-    }
+    books.forEach(book => UI.addBookToList(book));
+  }
 
-    static addBookToList(book) {
-        const list = document.querySelector('#book-list');
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
 
-        const row = document.createElement('tr');
+    const row = document.createElement("tr");
 
-        row.innerHTML = `
+    row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
         `;
 
-        list.appendChild(row);
-    }
+    list.appendChild(row);
+  }
 
-    static deleteBook(el) {
-        if(el.classList.contains('delete')) {
-            el.parentElement.parentElement.remove();
-        }
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
     }
+  }
 
-    static clearFields(){
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
-        document.querySelector('#isbn').value = '';
-    }
+  static showAlert(message, className){
+      const div = document.createElement('div');
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+      const container = document.querySelector('.container');
+      const form = document.querySelector('#book-form');
+      container.insertBefore(div,form);
+      // Remove in 5 seconds
+      setTimeout(() => document.querySelector('.alert').remove(),5000);
+  }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
 }
 
 // Event Listener
 
-document.addEventListener('DOMContentLoaded', UI.displayBooks)
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Add book
 
-document.querySelector('#book-form').addEventListener('submit', (e) => {
+document.querySelector("#book-form").addEventListener("submit", e => {
+  e.preventDefault();
 
-    e.preventDefault();
+  // Form Values
 
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const isbn = document.querySelector('#isbn').value;
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
 
+  // Validate
+  if (title === "" || author === "" || isbn == -"") {
+    UI.showAlert("Please fill in all required fields", "info");
+  } else {
     const book = new Book(title, author, isbn);
-
     UI.addBookToList(book);
-
-    // Clear fields
     UI.clearFields();
+  }
 });
 
 // Remove book
-document.querySelector('#book-list').addEventListener('click',(e) =>
-{
-    UI.deleteBook(e.target)
+document.querySelector("#book-list").addEventListener("click", e => {
+  UI.deleteBook(e.target);
 });
